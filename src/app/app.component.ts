ccda138c76af7aac085c2f519ad119f5b7025af3
unknown <per@brf.dk>
2018-10-20 16:11:24 +0200

    module 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-kursusplan',
+  template: `
+    <h1>Kursus Oversigt</h1>
+    <img src="/assets/images/angular.png" />
+  `
 })
-export class AppComponent {
-  title = 'ng-kursus';
-}
+export class AppComponent {}
